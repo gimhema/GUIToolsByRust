78f5217ac4412e9e845a4fa4ead185080448a7a4
kimmo <hemakim0215@naver.com>
2025-08-03 12:09:07 +0900

    .

--- EntityEditor/src/entity_manager/entity.rs
+++ EntityEditor/src/entity_manager/entity.rs
@@ -2,7 +2,12 @@ use serde::Deserialize;
 
 
 
-pub struct EntityBase {
-
-
+pub struct CharacterBase {
+    unique: u32,
+    name: String,
+    health: u32,
+    mana: u32,
+    stamina: u32,
+    attack_power: u32,
+    attack_speed: f32,
 }
--- EntityEditor/src/entity_manager/raw_data.rs
+++ EntityEditor/src/entity_manager/raw_data.rs
@@ -1,3 +1,27 @@
 use std::collections::HashMap;
+use serde::Deserialize;
+
+
+#[derive(Debug, Deserialize)]
+struct CharacterInfo {
+    unique: u32,
+    name: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct CharacterStatusInfo {
+    unique: u32,
+    health: u32,
+    mana: u32,
+    stamina: u32,
+}
+
+
+#[derive(Debug, Deserialize)]
+struct CharacterAttackInfo {
+    unique: u32,
+    attack_power: u32,
+    attack_speed: f32,
+}
 
 
