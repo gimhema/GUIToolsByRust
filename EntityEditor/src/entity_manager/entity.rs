71da5cff44d3311989fa78ce4c1655554e964c93
kimmo <hemakim0215@naver.com>
2025-08-03 12:34:26 +0900

    .

--- EntityEditor/src/entity_manager/entity.rs
+++ EntityEditor/src/entity_manager/entity.rs
@@ -1,7 +1,8 @@
 use serde::Deserialize;
 use super::raw_data::*;
 use std::collections::HashMap;
-
+use std::error::Error;
+use std::fs::File;
 
 pub trait EntityBox {
     fn load_data(&mut self) -> Result<(), Box<dyn std::error::Error>>;
@@ -17,12 +18,18 @@ pub struct CharacterEntity {
 
 pub struct CharacterEntityContainer {
     entities: HashMap<u32, CharacterEntity>,
+    characeter_info_data_path : String,
+    character_status_info_data_path: String,
+    character_attack_info_data_path: String,
 }
 
 impl CharacterEntityContainer {
     pub fn new() -> Self {
         CharacterEntityContainer {
             entities: HashMap::new(),
+            characeter_info_data_path: String::new(),
+            character_status_info_data_path: String::new(),
+            character_attack_info_data_path: String::new(),
         }
     }
 
@@ -41,7 +48,40 @@ impl CharacterEntityContainer {
 
 impl EntityBox for CharacterEntityContainer {
     fn load_data(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        // Load data logic here
+
+        {
+            let file = File::open(self.characeter_info_data_path.as_str())?;
+            let mut rdr = csv::Reader::from_reader(file);
+
+            for result in rdr.deserialize::<RawDataCharacterInfo>() {
+                // let record = result?;
+                // println!("{:?}", record);
+            }
+        }
+
+        {
+            let file = File::open(self.character_status_info_data_path.as_str())?;
+            let mut rdr = csv::Reader::from_reader(file);
+
+            for result in rdr.deserialize::<RawDataCharacterStatusInfo>() {
+                // let record = result?;
+                // println!("{:?}", record);
+            }
+        }
+
+        {
+            let file = File::open(self.character_attack_info_data_path.as_str())?;
+            let mut rdr = csv::Reader::from_reader(file);
+
+            for result in rdr.deserialize::<RawDataCharacterAttackInfo>() {
+                // let record = result?;
+                // println!("{:?}", record);
+            }
+        } 
+
+
+
+
         Ok(())
     }
 
